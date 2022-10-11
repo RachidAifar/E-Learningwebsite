a811171e37b7b2441999a60cccb6171abc15771a
RachidAifar <90982692+RachidAifar@users.noreply.github.com>
2022-10-11 16:29:54 +0200

    fix:first view has been implemented

--- main/urls.py
+++ main/urls.py
@@ -1,12 +1,7 @@
 from django.urls import path
-from . import views
-from django.contrib import admin
-from django.urls import include, path
 
-urlpatterns = [
-    path('E-Learningwebsite', include('main.urls')),
-    path('admin/', admin.site.urls),
-]
+from . import views
+
 urlpatterns = [
     path('', views.index, name='index'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. Finaaly")
+    return HttpResponse("Hello, world. You're here finally")
 
 
