ef8450df250cf1fb6a78ddb9f111829f6defb137
RachidAifar <90982692+RachidAifar@users.noreply.github.com>
2022-10-15 14:08:31 +0200

    feat:first model created

--- main/models.py
+++ main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Users(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    user_name = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    image = models.CharField(max_length=100)
+    dateOfJoining = models.DateField(auto_now_add=True)
+
