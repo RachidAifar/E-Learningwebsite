b14317306ba3f63914dc18cc8cf9f46c6a27867f
RachidAifar <90982692+RachidAifar@users.noreply.github.com>
2022-10-21 12:13:04 +0200

    feat<settings.py,urls.py>:the django rest_framework has been installed

--- eLearning/urls.py
+++ eLearning/urls.py
@@ -18,6 +18,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('main.urls')),
+    path('api-auth/', include('rest_framework.urls')),
     path('main/', include('main.urls')),
     path('admin/', admin.site.urls),
 ]
