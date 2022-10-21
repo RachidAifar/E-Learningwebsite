c3a782122a80d1452b8dac07156ee2ac44b328fb
RachidAifar <90982692+RachidAifar@users.noreply.github.com>
2022-10-21 11:30:48 +0200

    fix<admin.py>:class name 'sites' has been changed

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+admin.site.register(models.Users)
+admin.site.register(models.Teacher)
+admin.site.register(models.Course)
+admin.site.register(models.Student)
+admin.site.register(models.ContactInfo)
+admin.site.register(models.CourseCategory)
+admin.site.register(models.CourseVideo)
+admin.site.register(models.VideoInfo)
+admin.site.register(models.Quizzes)
+admin.site.register(models.CoursePDF)
