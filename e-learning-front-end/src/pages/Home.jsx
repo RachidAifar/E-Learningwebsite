78d4a61e53381016a54aeb6487eda5c65def128d
RachidAifar <90982692+RachidAifar@users.noreply.github.com>
2022-11-01 22:09:03 +0100

    feat<component>:header has been added

---
+++ e-learning-front-end/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+//import React from 'react'
+import React, { Fragment } from "react";
+import Header from "../components/Header/Header";
+
+const Home = () => {
+  return (
+    <Fragment>
+      <Header />
+    </Fragment>
+  )
+}
+
+export default Home
