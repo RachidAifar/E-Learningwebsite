a059c42fef712a181cddd0f14b65336502cf3dd9
RachidAifar <90982692+RachidAifar@users.noreply.github.com>
2022-11-04 18:11:40 +0100

    feat<componenet>:headr style has been modified

--- e-learning-front-end/src/pages/Home.jsx
+++ e-learning-front-end/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 //import React from 'react'
 import React, { Fragment } from "react";
-import Header from "../components/Header/Header";
+import Header from "../components/common/Header/Header";
 
 const Home = () => {
   return (
