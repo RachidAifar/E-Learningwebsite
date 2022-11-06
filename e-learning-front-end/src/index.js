141a7be08ef10728e5a0532231bbd77d24014b9a
RachidAifar <90982692+RachidAifar@users.noreply.github.com>
2022-11-06 10:33:38 +0100

    feat<index.js>:the render function changed

--- e-learning-front-end/src/index.js
+++ e-learning-front-end/src/index.js
@@ -1,20 +1,42 @@
 import React from 'react';
-import ReactDOM from "react-dom";
-//import ReactDOM from 'react-dom/client';
+
 
 import App from './App';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import reportWebVitals from './reportWebVitals';
 import "remixicon/fonts/remixicon.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
 
-ReactDOM.render(
-  <React.StrictMode>
+
+
+
+
+
+
+
+
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+root.render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </StrictMode>
 );
 
 
 
+
+
+
+//import ReactDOM from "react-dom";
+//import ReactDOM from 'react-dom/client';
+//import reportWebVitals from './reportWebVitals';
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
