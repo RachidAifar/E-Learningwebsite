e09aa056ea8871ca35fbc1838fb165141757f3c5
RachidAifar <90982692+RachidAifar@users.noreply.github.com>
2022-11-07 20:51:21 +0100

    feat<project.src>:the index styling changed

--- e-learning-front-end/src/pages/Home.jsx
+++ e-learning-front-end/src/pages/Home.jsx
@@ -1,12 +1,18 @@
 //import React from 'react'
 import React, { Fragment } from "react";
 import Header from "../components/common/Header/Header";
+import HeroSlider from "../components/HeroSlider/HeroSlider"
+//import HeroSection from "../components/HeroSection/HeroSection"
+import Cousres from "../components/Courses-section/Courses"
 
 const Home = () => {
   return (
-    <Fragment>
-      <Header />
-    </Fragment>
+  <Fragment>
+    
+    <Header />
+    <HeroSlider />
+    <Cousres />
+  </Fragment>
   )
 }
 
