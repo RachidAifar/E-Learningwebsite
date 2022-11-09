d22abe4d14486b54ff37cd18ee514fce88c53195
RachidAifar <90982692+RachidAifar@users.noreply.github.com>
2022-11-09 13:49:06 +0100

    feat<home>:the footer added to the main app

--- e-learning-front-end/src/pages/Home.jsx
+++ e-learning-front-end/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 //import React from 'react'
 import React, { Fragment } from "react";
 import Header from "../components/common/Header/Header";
+import Footer from "../components/common/footer/Footer";
 import HeroSlider from "../components/HeroSlider/HeroSlider"
 //import HeroSection from "../components/HeroSection/HeroSection"
 import Cousres from "../components/Courses-section/Courses"
@@ -8,10 +9,10 @@ import Cousres from "../components/Courses-section/Courses"
 const Home = () => {
   return (
   <Fragment>
-    
     <Header />
     <HeroSlider />
     <Cousres />
+    <Footer />
   </Fragment>
   )
 }
