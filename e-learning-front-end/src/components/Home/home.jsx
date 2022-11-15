4a076602d276dc9e371e0f78b8829bfb48056a5f
RachidAifar <90982692+RachidAifar@users.noreply.github.com>
2022-11-15 11:25:01 +0100

    fix<component>:the location has been changed

---
+++ e-learning-front-end/src/components/Home/home.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import HeroSlider from "./HeroSlider/HeroSlider"
+//import HeroSection from "../components/HeroSection/HeroSection"
+import Cousres from "./Courses-section/Courses"
+
+const Home = () => {
+  return (
+   <>
+    <HeroSlider />
+    <Cousres />
+   </> 
+  )
+}
+
+export default Home
