7c53b7966e6d053ee14fc945e02416237fb69720
RachidAifar <90982692+RachidAifar@users.noreply.github.com>
2022-11-15 11:25:33 +0100

    feat<component>:the data has been created

---
+++ e-learning-front-end/src/components/Data/Data.jsx
@@ -0,0 +1,27 @@
+export const navLinks = [
+    {
+      text: "Home",
+      path: "/",
+    },
+    {
+      text: "Courses",
+      path: "/courses",
+    },
+  
+    {
+      text: "Teachers",
+      path: "/teachers",
+    },
+    {
+      text: "MyCourses",
+      path: "/mycourses",
+    },
+    {
+      text: "Course categories",
+      path: "/coursecategory",
+    },
+    {
+      text: "About",
+      path: "about",
+    },
+  ]
