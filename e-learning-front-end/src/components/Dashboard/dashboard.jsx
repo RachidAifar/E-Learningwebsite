ab2c8b798dae3f0c3c876214db4970c76bbe28de
RachidAifar <90982692+RachidAifar@users.noreply.github.com>
2022-12-17 01:04:00 +0100

    feat<component>:dashboard has been implemented

--- e-learning-front-end/src/components/Dashboard/dashboard.jsx
+++ e-learning-front-end/src/components/Dashboard/dashboard.jsx
@@ -1,25 +1,14 @@
 import React from "react";
-import {Link} from 'react-router-dom';
-import MyCourses from "./MyCourses";
+import Sidebar from "./Sidebar";
 const Dashboard = () => {
   return (
     <div className="container mt-4">
         <div className="row">
             <aside className='col-md-3'>
-                <div className="card">
-                    <h5 className="card-header">Dashboard</h5>
-                    <div className="list-group list-group-flush">
-                        <Link to={'/mycourses'} className="list-group-item list-group-action ">My Courses</Link>
-                        <Link to={'/all-courses'} className="list-group-item list-group-action ">All Courses</Link>
-                        <Link to={'/recommended-courses'} className="list-group-item list-group-action ">Recommended Courses</Link>
-                        <Link to={'/profil-setting'} className="list-group-item list-group-action ">Profile Setting</Link>
-                        <Link to={'/chnage-password'} className="list-group-item list-group-action ">Change Password</Link>
-                        <Link to={'/logout'} className="list-group-item list-group-action text-danger">Logout</Link>
-                    </div>
-                </div>    
+                <Sidebar/>
             </aside>
             <section className="col-md-9">
-                <MyCourses />
+                Dashboard
             </section>
         </div>
     </div>
--- e-learning-front-end/src/components/pages/Pages.jsx
+++ e-learning-front-end/src/components/pages/Pages.jsx
@@ -8,6 +8,12 @@ import Footer from "../common/footer/Footer";
 import About from "../About/About";
 import Login from "../Authontication_Forms/Login_form"
 import Register from "../Authontication_Forms/Register_form"
+import Dashboard from "../Dashboard/dashboard"
+import MyCourses from "../Dashboard/MyCourses";
+import FavoriteCourses from "../Dashboard/FavoriteCourses";
+import RecommendedCourses from "../Dashboard/RecommendedCourses";
+import ProfileSetting from "../Dashboard/ProfileSetting";
+import ChangePassword from "../Dashboard/ChangePassword";
 
 //import HeroSlider from "../components/HeroSlider/HeroSlider"
 //import HeroSection from "../components/HeroSection/HeroSection"
@@ -25,6 +31,12 @@ const Pages = () => {
           <Route exact path='/about' element={<About />} />
           <Route exact path='/login' element={<Login />} />
           <Route exact path='/register' element={<Register />} />
+          <Route exact path='/dashboard' element={<Dashboard />} />
+          <Route exact path='/my-courses' element={<MyCourses />} />
+          <Route exact path='/favorite-courses' element={<FavoriteCourses />} />
+          <Route exact path='/recommended-courses' element={<RecommendedCourses />} />
+          <Route exact path='/profil-setting' element={<ProfileSetting />} />
+          <Route exact path='/chnage-password' element={<ChangePassword />} />
           <Route exact path='/CourseDetail/:course_id' element={<CourseDetail/>} />
         </Routes>
         <Footer />
