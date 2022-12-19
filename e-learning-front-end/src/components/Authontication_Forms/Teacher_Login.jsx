c86e7827fca6e545ede8cf18ec8fb33af1cd82b6
RachidAifar <90982692+RachidAifar@users.noreply.github.com>
2022-12-19 18:09:59 +0100

    feat<component>:teacher autho forms added

---
+++ e-learning-front-end/src/components/Authontication_Forms/Teacher_Login.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import {Link} from 'react-router-dom';
+import "./Login.css";
+const Login = () => {
+    return (
+        <section className="sm-image">
+            <div className="container-fluid h-custom">
+                <div className="row d-flex justify-content-center align-items-center">
+                    <div className="col-md-5 col-lg-6 col-sm-5">
+                        <img src={`https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp`}
+                            className="img-fluid" alt=""></img>
+                    </div>
+                    <div className="card col-md-5 col-md-4">
+                        <div className="card-body">
+                        <h3 className="text-center mb-3">Teacher Login</h3>
+                            <form>
+                                {/* <!-- Email input --> */}
+                                <div className="form-outline mb-2">
+                                    <input type="email" id="form3Example3" className="form-control form-control-sm"
+                                        placeholder="Enter a valid email address" />
+                                    <label className="form-label" htmlFor="form3Example3">Email address</label>
+                                </div>
+
+                                {/* <!-- Password input --> */}
+                                <div className="form-outline mb-2">
+                                    <input type="password" id="form3Example4" className="form-control form-control-sm"
+                                        placeholder="Enter password" />
+                                    <label className="form-label" htmlFor="form3Example4">Password</label>
+                                </div>
+
+                                <div className="d-flex justify-content-between align-items-center">
+                                    {/* <!-- Checkbox --> */}
+                                    <div className="form-check mb-0">
+                                        <input className="form-check-input me-2" type="checkbox" value="" id="form2Example3" />
+                                        <label className="form-check-label" htmlFor="form2Example3">
+                                            Remember me
+                                        </label>
+                                    </div>
+                                    <Link to={"#!"} className="text-body">Forgot password?</Link>
+                                </div>
+
+                                <div className="text-center text-md-start mt-4 pt-2">
+                                    <button type="button" className="btn btn-primary btn-sm">Login</button>
+                                    <p className="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <Link to={"/register"}
+                                        className="link-danger">Register</Link></p>
+                                </div>
+
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+           
+        </section>
+    );
+};
+export default Login;
---
+++ e-learning-front-end/src/components/Authontication_Forms/Teacher_Register.jsx
@@ -0,0 +1,59 @@
+import React  from 'react';  
+import { Link } from 'react-router-dom';      
+import './Register.css'       
+        
+        
+const Register = () => {
+    return (        
+        <section className="sm-image">
+            <div className="mask d-flex align-items-center h-100 gradient-custom-3">
+                <div className="container h-100">
+                    <div className="row d-flex justify-content-center align-items-center h-100">
+                        <div className="col-5 col-md-5 col-lg-5 col-md-2">
+                            <div className="card" >
+                                <div className="card-body">
+                                    <h2 className="text-uppercase text-center mb-5">Teacher Register</h2>
+                                    <form>
+                                        <div className="form-outline mb-2">
+                                            <input type="text" id="form3Example1cg" className="form-control form-control-sm" />
+                                            <label className="form-label" htmlFor="form3Example1cg">Your Name</label>
+                                        </div>
+
+                                        <div className="form-outline mb-2">
+                                            <input type="email" id="form3Example3cg" className="form-control form-control-sm" />
+                                            <label className="form-label" htmlFor="form3Example3cg">Your Email</label>
+                                        </div>
+
+                                        <div className="form-outline mb-2">
+                                            <input type="password" id="form3Example4cg" className="form-control form-control-sm" />
+                                            <label className="form-label" htmlFor="form3Example4cg">Password</label>
+                                        </div>
+
+                                        <div className="form-outline mb-2">
+                                            <input type="password" id="form3Example4cdg" className="form-control form-control-sm" />
+                                            <label className="form-label" htmlFor="form3Example4cdg">Repeat your password</label>
+                                        </div>
+                                        <div className="form-outline mb-2">
+                                            <textarea type="email" id="form3Example3cg" className="form-control" />
+                                            <label className="form-label" htmlFor="form3Example3cg">Speciality</label>
+                                        </div>
+                                        <div className="d-flex justify-content-center">
+                                            <button type="button"
+                                                className="btn btn-success btn-block btn-lg gradient-custom-4 text-body">Register</button>
+                                            </div>
+
+                                        <p className="text-center text-muted mt-2 mb-0">Have already an account? <Link to={"/login"}
+                                            className="fw-bold text-body"><u>Login here</u></Link></p>
+                      
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+export default Register;
+    
