bc6e30f5004d44fa515b782e4fd4d4d84a41df22
RachidAifar <90982692+RachidAifar@users.noreply.github.com>
2022-12-23 17:51:21 +0100

    feat<componenet>:login register | post data by axiox

--- e-learning-front-end/src/components/Authontication_Forms/Teacher_Login.jsx
+++ e-learning-front-end/src/components/Authontication_Forms/Teacher_Login.jsx
@@ -1,10 +1,61 @@
 import React from "react";
+import {useEffect,useState } from 'react';  
+import axios from 'axios';
 import {Link} from 'react-router-dom';
 import "./Login.css";
+
+
+
+
+const baseUrl='http://127.0.0.1:8000/api';
+
+
 const Login = () => {
+    const [teacherLoginData, setTeacherLoginData] = useState({ //the same of register you can see the register.
+        'email':'',
+        'password':''
+    });
+    //change element value
+    const handleChange=(event)=>{
+        setTeacherLoginData({
+            ...teacherLoginData,
+            [event.target.name]:event.target.value
+        });
+    };
+    const submitForm=()=>{
+        const teacherFormData =new FormData();
+        teacherFormData.append("email", teacherLoginData.email)
+        teacherFormData.append("password", teacherLoginData.password)
+       
+
+         try{
+            axios.post(baseUrl+'/teacher_login',teacherFormData).then((response)=>{
+                console.log(response.data);
+                if(response.data.bool===true){
+                localStorage.setItem('teacherLoginStatus', true);
+                window.location.href='/teacher_dashboard';
+            }
+             });
+
+        }catch(error){
+            console.log(error);
+        }
+    }; 
+
+    const teacherLoginStatus= localStorage.getItem('teacherLoginStatus');//the redirect the loged in user to dashboard 
+    if(teacherLoginStatus==='true'){
+        window.location.href='/teacher_dashboard'
+    }
+
+
+    useEffect(()=>{
+        document.title='Teacher Login';
+    });
     return (
         <section className="sm-image">
-            <div className="container-fluid h-custom">
+            <div className="container-fluid h-custom  h-100">
+            {/* {teacherData.status==='auccess' && <p className='text-success'>you register successfully</p>}
+            {teacherData.status==='error' && <p className='text-danger'>something went wrong!! try again</p>} */}
                 <div className="row d-flex justify-content-center align-items-center">
                     <div className="col-md-5 col-lg-6 col-sm-5">
                         <img src={`https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp`}
@@ -16,14 +67,14 @@ const Login = () => {
                             <form>
                                 {/* <!-- Email input --> */}
                                 <div className="form-outline mb-2">
-                                    <input type="email" id="form3Example3" className="form-control form-control-sm"
+                                    <input value={teacherLoginData.email} onChange={handleChange} name="email" type="email" id="form3Example3" className="form-control form-control-sm"
                                         placeholder="Enter a valid email address" />
                                     <label className="form-label" htmlFor="form3Example3">Email address</label>
                                 </div>
 
                                 {/* <!-- Password input --> */}
                                 <div className="form-outline mb-2">
-                                    <input type="password" id="form3Example4" className="form-control form-control-sm"
+                                    <input value={teacherLoginData.password} onChange={handleChange} name="password"  type="password" id="form3Example4" className="form-control form-control-sm"
                                         placeholder="Enter password" />
                                     <label className="form-label" htmlFor="form3Example4">Password</label>
                                 </div>
@@ -40,7 +91,7 @@ const Login = () => {
                                 </div>
 
                                 <div className="text-center text-md-start mt-4 pt-2">
-                                    <button type="button" className="btn btn-primary btn-sm">Login</button>
+                                    <button type="button" onClick={submitForm} className="btn btn-primary btn-sm">Login</button>
                                     <p className="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <Link to={"/register"}
                                         className="link-danger">Register</Link></p>
                                 </div>
--- e-learning-front-end/src/components/Authontication_Forms/Teacher_Register.jsx
+++ e-learning-front-end/src/components/Authontication_Forms/Teacher_Register.jsx
@@ -1,50 +1,99 @@
 import React  from 'react';
-import { useEffect } from 'react';  
+import {useState } from 'react';  
+import axios from 'axios';
 import { Link } from 'react-router-dom';      
 import './Register.css'       
         
-        
+const baseUrl='http://127.0.0.1:8000/api/teacher/';
+
 const Register = () => {
-    useEffect(()=>{
-            document.title='Teacher Register'
+    const [teacherData, setTeacherData] = useState({ //the data will by taken from the input and put it into teacherData after make axios post
+        'teacher_fullname':'',                       //handleChange will store all the changes which are targeted 
+        'password':'',                               //submitForm we will append each value from input "form"
+        'email':'',
+        'mobile_phone':'',
+        'address':'',
+        'speciality':'',
+        'status':''
     });
+    //change element value
+    const handleChange=(event)=>{
+        setTeacherData({
+            ...teacherData,
+            [event.target.name]:event.target.value
+        });
+    };
+    //end
+    const submitForm=()=>{
+        const teacherFormData =new FormData();
+        teacherFormData.append("teacher_fullname", teacherData.teacher_fullname)
+        teacherFormData.append("password", teacherData.password)
+        teacherFormData.append("email", teacherData.email)
+        teacherFormData.append("mobile_phone", teacherData.mobile_phone)
+        teacherFormData.append("address", teacherData.address)
+        teacherFormData.append("speciality", teacherData.speciality)
+
+        try{
+            axios.post(baseUrl,teacherFormData,).then((response)=>{
+                setTeacherData({
+                    'teacher_fullname':'',
+                    'password':'',
+                    'email':'',
+                    'mobile_phone':'',
+                    'address':'',
+                    'speciality':'',
+                    'status':'auccess'
+                });
+            });
+        }catch(error){
+            setTeacherData({'status':'error'});
+        }
+    }; 
+    const teacherLoginStatus= localStorage.getItem('teacherLoginStatus');//if the user is logied in redirect them to teacher dashboard.
+    if(teacherLoginStatus==='true'){
+        window.location.href='/teacher_dashboard'
+    }
+
+
     return (        
         <section className="sm-image">
-            <div className="mask d-flex align-items-center h-100 gradient-custom-3">
+            {teacherData.status==='auccess' && <p className='text-success'>you register successfully</p>}
+            {teacherData.status==='error' && <p className='text-danger'>something went wrong!! try again</p>}
+            <div className="mask d-flex align-items-center gradient-custom-3">
                 <div className="container h-100">
-                    <div className="row d-flex justify-content-center align-items-center h-100">
+                    <div className="row d-flex justify-content-center align-items-center">
                         <div className="col-5 col-md-5 col-lg-5 col-md-2">
                             <div className="card" >
                                 <div className="card-body">
                                     <h2 className="text-uppercase text-center mb-5">Teacher Register</h2>
                                     <form>
                                         <div className="form-outline mb-2">
-                                            <input type="text" id="form3Example1cg" className="form-control form-control-sm" />
+                                            <input value={teacherData.teacher_fullname} onChange={handleChange} name="teacher_fullname" type="text" id="form3Example1cg" className="form-control form-control-sm" />
                                             <label className="form-label" htmlFor="form3Example1cg">Your Name</label>
                                         </div>
                                         <div className="form-outline mb-2">
-                                            <input type="password" id="form3Example4cg" className="form-control form-control-sm" />
+                                            <input value={teacherData.password} onChange={handleChange}  name="password" type="password" id="form3Example4cg" className="form-control form-control-sm" />
                                             <label className="form-label" htmlFor="form3Example4cg">Password</label>
                                         </div>
                                         <div className="form-outline mb-2">
-                                            <input type="email" id="form3Example3cg" className="form-control form-control-sm" />
+                                            <input value={teacherData.email} onChange={handleChange}  name="email" type="email" id="form3Example3cg" className="form-control form-control-sm" />
                                             <label className="form-label" htmlFor="form3Example3cg">Your Email</label>
                                         </div>
                                         <div className="form-outline mb-2">
-                                            <input type="number" id="form3Example4cdg" className="form-control form-control-sm" />
+                                            <input value={teacherData.mobile_phone} onChange={handleChange}  name ="mobile_phone" type="number" id="form3Example4cdg" className="form-control form-control-sm" />
                                             <label className="form-label" htmlFor="form3Example4cdg">Mobile Phone</label>
                                         </div>
                                         <div className="form-outline mb-2">
-                                            <textarea type="text" id="form3Example3cg" className="form-control" />
+                                            <textarea value={teacherData.address} onChange={handleChange}  name="address" type="text" id="form3Example3cg" className="form-control" />
                                             <label className="form-label" htmlFor="form3Example3cg">Address</label>
                                         </div>
                                         <div className="form-outline mb-2">
-                                            <textarea type="text" id="form3Example3cg" className="form-control" />
+                                            <textarea value={teacherData.speciality} onChange={handleChange}  name="speciality" type="text" id="form3Example3cg" className="form-control" />
                                             <label className="form-label" htmlFor="form3Example3cg">Speciality</label>
                                         </div>
                                         <div className="d-flex justify-content-center">
-                                            <button type="button"
-                                                className="btn btn-success btn-block btn-lg gradient-custom-4 text-body">Register</button>
+                                            <button onClick={submitForm} type="button"
+                                                className="btn btn-block btn-lg gradient-custom-4 text-body">Register</button>
                                             </div>
                                         <p className="text-center text-muted mt-2 mb-0">Have already an account? <Link to={"/login"}
                                             className="fw-bold text-body"><u>Login here</u></Link></p>
