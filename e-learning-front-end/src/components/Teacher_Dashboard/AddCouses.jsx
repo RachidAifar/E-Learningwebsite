cff92c8ffdc9c76de5373eb08a916a27938fb309
RachidAifar <90982692+RachidAifar@users.noreply.github.com>
2022-12-25 01:11:54 +0100

    feat<componenet>:add couse has been impl with send post req by axios

--- e-learning-front-end/src/components/Teacher_Dashboard/AddCouses.jsx
+++ e-learning-front-end/src/components/Teacher_Dashboard/AddCouses.jsx
@@ -1,6 +1,72 @@
 import React from "react";
+import axios from 'axios';
 import Sidebar from "./T_Sidebar";
+import {useState,useEffect} from "react";
+
+
+const baseUrl='http://127.0.0.1:8000/api';
+
 const AddCourse = () => {
+    const [cats, setCtas]=useState([]);
+    const [courseData, setCourseData] =useState({
+        category:'',
+        course_title:'',
+        course_description:'',
+        feature_img:'',
+        technologies:''
+    });
+
+    useEffect(()=>{
+        try{
+            axios.get(baseUrl+'/courseCategory').then((response)=>{
+                setCtas(response.data);
+                // setCtas({
+                //     cats:response.data,
+                // });
+            });
+
+        }catch(error){
+            console.log(error);
+        }
+    },[]);
+    const handleChange=(event)=>{
+        setCourseData({
+            ...courseData,
+            [event.target.name]:event.target.value
+        });
+    };
+    const handleFileChange=(event)=>{
+        setCourseData({
+            ...courseData,
+            [event.target.name]:event.target.files[0]
+        });
+    };
+
+    const submitForm=()=>{
+        const   CourseFormData =new FormData();
+        CourseFormData.append("category",courseData.category );
+        CourseFormData.append("teacher", 1);
+        CourseFormData.append("course_title",courseData.course_title );
+        CourseFormData.append("course_description",courseData.course_description );
+        CourseFormData.append("feature_img",courseData.feature_img,courseData.feature_img.name );
+        CourseFormData.append("technologies",courseData.technologies );
+       
+
+         try{
+            axios.post(baseUrl+'/course/',CourseFormData,{
+                headers: {
+                    'content-type':'multipart/form-data'
+                }
+            })
+            .then((res)=>{
+                console.log(res.data);
+            });
+        }catch(error){
+            console.log(error);
+        }
+    }; 
+
+
   return (
     <div className="container mt-4">
             <div className="row">
@@ -11,40 +77,42 @@ const AddCourse = () => {
                 <div className="card"> 
                     <h4 className="card-header">Add Course</h4>   
                     <div className="card-body">
-                    <div class="mb-2 row">
-                        <label for="staticEmail" class="col-sm-2 col-form-label">Title</label>
-                        <div class="col-sm-10">
-                        <input type="text" readonly class="form-control" id="staticEmail" />
+                    <div className="mb-2 row">
+                        <label htmlFor="inputPassword" className="col-sm-2 col-form-label">Category</label>
+                        <div className="col-sm-10">
+                            <select onChange={handleChange} name="category" type="text" className="form-control">
+                                {cats.map((category,index)=>{ return <option key={index} value={category.category_id}>{category.title}</option>})} 
+                            </select>
+                        </div>
+                    </div>
+                    <div className="mb-2 row">
+                        <label htmlFor="staticEmail" className="col-sm-2 col-form-label">Title</label>
+                        <div className="col-sm-10">
+                        <input onChange={handleChange} name="course_title" type="text" className="form-control" id="staticEmail" />
                         </div>
                     </div>
                     
-                    <div class="mb-2 row">
-                        <label for="inputPassword" class="col-sm-2 col-form-label">Description</label>
-                        <div class="col-sm-10">
-                        <textarea type="text" class="form-control" id="inputPassword"/>
+                    <div className="mb-2 row">
+                        <label htmlFor="inputPassword" className="col-sm-2 col-form-label">Description</label>
+                        <div className="col-sm-10">
+                        <textarea onChange={handleChange} name="course_description" type="text" className="form-control" id="inputPassword"/>
                         </div>
                     </div>
-                    <div class="mb-3 row">
-                        <label for="inputPassword" class="col-sm-2 col-form-label">Course Video</label>
-                        <div class="col-sm-10">
-                        <input type="file" class="form-control" id="inputPassword"/>
+                    <div className="mb-3 row">
+                        <label htmlFor="inputPassword" className="col-sm-2 col-form-label">Featured Image</label>
+                        <div className="col-sm-10">
+                        <input onChange={handleFileChange} name="feature_img" type="file" className="form-control" id="inputPassword"/>
                         </div>
                     </div>
-                    <div class="mb-2 row">
-                        <label for="inputPassword" class="col-sm-2 col-form-label">Category</label>
-                        <div class="col-sm-10">
-                        <input type="text" class="form-control" id="inputPassword"/>
-                        </div>
-                    </div>
-                    <div class="mb-2 row">
-                        <label for="inputPassword" class="col-sm-2 col-form-label">Technologies</label>
-                        <div class="col-sm-10">
-                        <input type="text" class="form-control" id="inputPassword"/>
+                    <div className="mb-2 row">
+                        <label htmlFor="inputPassword" className="col-sm-2 col-form-label">Technologies</label>
+                        <div className="col-sm-10">
+                        <input onChange={handleChange} name="technologies" type="text" className="form-control" placeholder="Python,Java,C#,JavaScript,HTML,CSS" id="inputPassword"/>
                         </div>
                     </div>
                     <hr />
-                    <div class="mb-5">
-                    <button type="button" class="btn btn-primary">submit</button>                    
+                    <div className="mb-5">
+                    <button onClick={submitForm} type="button" className="btn btn-primary">submit</button>                    
                     </div>
                     </div>    
                 </div>
