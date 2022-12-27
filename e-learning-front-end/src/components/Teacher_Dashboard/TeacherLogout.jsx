172d621ba6e97506318b2019a8ceee83c2e5c9f9
RachidAifar <90982692+RachidAifar@users.noreply.github.com>
2022-12-27 13:47:27 +0100

    feat<componenet>:logout has been imple

--- e-learning-front-end/src/components/Teacher_Dashboard/TeacherLogout.jsx
+++ e-learning-front-end/src/components/Teacher_Dashboard/TeacherLogout.jsx
@@ -2,7 +2,8 @@
 
 
 const Logout =() =>{
-    localStorage.removeItem('teacherLoginStatus');//the redirect the loged in user to dashboard 
+    localStorage.removeItem('teacherLoginStatus');//the redirect the loged in user to dashboar
+    localStorage.removeItem('teacher_id');
     window.location.href='/teacher_login';
     return(
         <div>
