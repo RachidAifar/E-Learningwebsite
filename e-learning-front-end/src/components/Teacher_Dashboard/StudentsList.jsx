5156326015abc92b3ac6402b072fc2ccd31cea7d
RachidAifar <90982692+RachidAifar@users.noreply.github.com>
2023-01-16 20:06:00 +0100

    feat<component>:t_dashboard dispaly all enrolled student

--- e-learning-front-end/src/components/Teacher_Dashboard/StudentsList.jsx
+++ e-learning-front-end/src/components/Teacher_Dashboard/StudentsList.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
-import Sidebar from './T_Sidebar'
+import axios from 'axios';
+import {Link, useParams} from 'react-router-dom';
+import Sidebar from './T_Sidebar';
+import Swal from 'sweetalert2';
+import { useState,useEffect } from 'react';
 
 
+const baseUrl='http://127.0.0.1:8000/api';
 
-const StudentList =()=>{
+
+const StudentsList =()=>{
+    const [studentData, setStudentData] =useState([]);
+    const teacher_id= localStorage.getItem('teacher_id');
+    useEffect(()=>{
+        //fetch courses
+        try{
+            axios.get(baseUrl+'/fetch_allenrolled_students/'+teacher_id).then((response)=>{//getting teacher by id
+                setStudentData(response.data);
+            });
+
+        }catch(error){
+            console.log(error);
+        }
+    },[teacher_id]);
+    
     return(
         <div className="container mt-4">
             <div className="row">
@@ -13,31 +32,35 @@ const StudentList =()=>{
                 </aside>
                 <section className="col-md-9">    
                 <div className="card">
-                    <h4 className="card-header">Students List</h4>
+                    <h4 className="card-header">All Students List</h4>
                     <div className="card-body">
                         <table className="table table-bordered ">
                             <thead>
                                 <tr>
                                     <th>Name</th>
-                                    <th>Enrolled Course</th>
-                                    <th>Action</th>
+                                    <th>Email</th>
+                                    <th>date of enrolling</th>
+                                    <th>Mobil Phone</th>
                                 </tr>
                             </thead>
                             <tbody>
+                                {studentData.map((student,index)=>  
                                 <tr>
-                                    <td>Rachid Aifar</td>
-                                    <td><Link to={""}>Java Development</Link></td>
+                                    <td>{student.student.student_fullname}</td>
+                                    <td >{student.student.email}</td> {/*add imagee from database*/}
+                                    <td>{student.enrollment_date}</td>
                                     <td>
-                                        <button className="btn btn-danger btn-sm mb-2  mt-0 2 ms-2" >Delete</button>
+                                        {student.student.mobile_phone}
                                     </td>
                                 </tr>
+                                )}
                             </tbody>
                         </table>
                     </div>
                 </div>
                 </section>
             </div>
-        </div>        
+        </div>         
     )
 }
-export default StudentList;
+export default StudentsList;
