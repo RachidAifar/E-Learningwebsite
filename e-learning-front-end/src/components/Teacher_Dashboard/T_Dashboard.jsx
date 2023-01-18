a9e440d0e3f4315c7371ab1fa92cbc6b10e3c983
RachidAifar <90982692+RachidAifar@users.noreply.github.com>
2023-01-18 13:22:12 +0100

    feat<compoenet>:main page of teacherDashboard implem

--- e-learning-front-end/src/components/Teacher_Dashboard/T_Dashboard.jsx
+++ e-learning-front-end/src/components/Teacher_Dashboard/T_Dashboard.jsx
@@ -1,6 +1,37 @@
 import React from "react";
 import Sidebar from "./T_Sidebar";
+import {Link} from 'react-router-dom';
+import axios from 'axios';
+import {useState,useEffect} from "react";
+
+
+
+
+
+const baseUrl='http://127.0.0.1:8000/api';
+
+
+
+
+
 const TeacherDashboard = () => {
+    const[teacherDashboar,setTeacherDashboar]=useState([]);
+    const teacher_id =localStorage.getItem('teacher_id');
+
+    useEffect(()=>{
+      //fetch courses
+      try{
+          axios.get(baseUrl+'/teacher/teacher_dashboard/'+teacher_id).then((response)=>{//geting teacher by id
+           console.log(response.data);
+           setTeacherDashboar(response.data);
+          });
+
+      }catch(error){
+          console.log(error);
+      }
+  },[teacher_id]);
+
+
   return (
     <div className="container mt-4">
         <div className="row">
@@ -8,9 +39,32 @@ const TeacherDashboard = () => {
                 <Sidebar/>
             </aside>
             <section className="col-md-9">
-              <div className="card">
-                <h5 className="card-header ">Dashboard</h5>
-              </div>  
+               <div className="row">
+                  <div className="col-md-4">
+                    <div className="card border-primary">
+                      <h4 className="card-header bg-primary text-white">Total Courses Uploaded</h4>
+                      <div className="card-body">
+                        <h3><Link to={"/teacher_courses"}>{teacherDashboar.total_teacher_courses}</Link></h3>
+                      </div>
+                    </div>   
+                  </div>
+                  <div className="col-md-4">
+                    <div className="card border-primary">
+                      <h4 className="card-header bg-success text-white">Total Students Enrolled</h4>
+                      <div className="card-body">
+                        <h3><Link to={"/students_list"}>{teacherDashboar.total_teacher_student}</Link></h3>
+                      </div>
+                    </div>   
+                  </div>
+                  <div className="col-md-4">
+                    <div className="card border-primary">
+                      <h4 className="card-header bg-warning text-white">Total Chapters Created</h4>
+                      <div className="card-body">
+                        <h3><Link to={"/teacher_courses"}>{teacherDashboar.total_teacher_chapters}</Link></h3>
+                      </div>
+                    </div>   
+                  </div>
+               </div>
             </section>
         </div>
     </div>
--- e-learning-front-end/src/components/Teacher_Dashboard/TeacherDetail.jsx
+++ e-learning-front-end/src/components/Teacher_Dashboard/TeacherDetail.jsx
@@ -1,52 +1,82 @@
-import {Link} from 'react-router-dom'
-import React from "react"
+import {Link, useParams} from 'react-router-dom'
+import React from "react";
+import axios from 'axios';
+import { useState,useEffect } from 'react';
+
+
+
 import ListGroup from 'react-bootstrap/ListGroup';
 import { Container, Row, Col } from "reactstrap";
 import Card from 'react-bootstrap/Card';
 import TeacherImage from "../images/202100527_1640328372828097_4475152047372531726_n.jpg";
 import "./teacherDetail.css"
+
+
+
+
+
+
+const baseUrl='http://127.0.0.1:8000/api';
+
+
 const TeacherDetail = () => {
+  const [teacherData, setteacherData] =useState([]);
+  const [courseData, setCourseData] =useState([]);
+  const [skillsData, setskillsData] =useState([]);
+
+  let {teacher_id}=useParams();
+
+  useEffect(()=>{
+    //fetch courses
+    try{
+        axios.get(baseUrl+'/teacher/'+teacher_id).then((response)=>{//geting teacher by id
+          setteacherData(response.data);
+          setCourseData(response.data.teacher_courses);
+          setskillsData(response.data.skills_list);
+        });
+
+    }catch(error){
+        console.log(error);
+    }
+},[teacher_id]);
+
+
     return (
     <section>
-      <Container className="card">
+      <Container className="card mt-5">
         <Row>
-          <Col lg="5" md="5">
+          <Col lg="5" md="5" className='mt-4'>
             <div className="thumbail">
               <img src={TeacherImage} alt="Teacher_Image" className="w-100" />
             </div>
           </Col>
 
-          <Col md="7">
+          <Col md="7" className='mt-4'>
             <div className="detail__content">
-              <h3>Rachid Aifar</h3>
-              <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Excepturi cupiditate animi deserunt libero nesciunt corporis
-                explicabo nobis ex quo molestiae!
+              <h3>{teacherData.teacher_fullname}</h3>
+              <p>{teacherData.bio}</p>
+              <p className="fw-bold">Speciality: <Link to={"/teacher_detail/:teacher_id"}>{teacherData.speciality}</Link></p>
+              <p className="fw-bold">Skills:&nbsp; 
+              {skillsData.map((skills,index) =>
+                <>
+                  <Link className="badge bg-warning ms-1" to={`/teacher_skill_course/${skills.trim()}/${teacherData.teacher_id}`}>{skills}</Link>
+                </>
+              )}
               </p>
-              <p className="fw-bold">Speciality: <Link to={"/teacher_detail:teacher_id"}>Software engineer</Link></p>
-              <p className="fw-bold">Skills: 
-              <Link to={"/category/c#"}>C#</Link> , 
-              <Link to={"/category/java"}>JAVA</Link>,
-              <Link to={"/category/python"}>Python</Link></p>
               <p className="fw-bold">Total Courses: 5</p>
               <p className="fw-bold">Rating 4/5</p> 
             </div>
           </Col>
         </Row>
         <div className="listItem mb-3">
-        <Card style={{ width: '58rem'  }}>
-          <Card.Header><h4>Courses List</h4></Card.Header>
-            <ListGroup variant="flush">
-              <ListGroup.Item><Link to={"/CourseDetail/:course_id/"}> Python Course 1</Link></ListGroup.Item>
-              <ListGroup.Item><Link to={"/CourseDetail/:course_id/"}> Python Course 2</Link></ListGroup.Item>
-              <ListGroup.Item><Link to={"/CourseDetail/:course_id/"}> Python Course 3</Link></ListGroup.Item>
-              <hr />
-              <ListGroup.Item><Link to={"/CourseDetail/:course_id/"}> Django Course 1</Link></ListGroup.Item>
-              <ListGroup.Item><Link to={"/CourseDetail/:course_id/"}> Django Course 2</Link></ListGroup.Item>
-              <ListGroup.Item><Link to={"/CourseDetail/:course_id/"}> Django Course 3</Link></ListGroup.Item>
-            </ListGroup>
-          </Card>
+          <div className='card mt-4'>
+            <h5 className='card-header'>Course List</h5>
+            <div className='list-group list-group-flush'>
+              {courseData.map((course,index)=>
+                <Link to={`/CourseDetail/${course.course_id}`} className="list-group-item list-group-item-action">{course.course_title}</Link>
+              )}
+            </div>
+          </div>
         </div>
       </Container>
     </section>
