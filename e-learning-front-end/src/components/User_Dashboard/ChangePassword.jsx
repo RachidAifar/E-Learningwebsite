8b5daea08075b8da06301b065c09f7fc1daef622
RachidAifar <90982692+RachidAifar@users.noreply.github.com>
2023-01-30 15:03:47 +0100

    feat<component>:change password implemented for student

--- e-learning-front-end/src/components/User_Dashboard/ChangePassword.jsx
+++ e-learning-front-end/src/components/User_Dashboard/ChangePassword.jsx
@@ -1,7 +1,67 @@
 import React from "react";
 import Sidebar from "./Sidebar";
+import axios from 'axios';
+import {useState} from "react";
+import Swal from 'sweetalert2';
+
+
+
+
+const baseUrl='http://127.0.0.1:8000/api';
+
+
+
+
+
+
 const ChangePassword = () => {
+
+
+    const [studentData, setStudentData] = useState({ //the data will by taken from the input and put it into teacherData after make axios post
+        'password':'',                       //handleChange will store all the changes which are targeted         //submitForm we will append each value from input "form"'
+    });
+    const student_id =localStorage.getItem('student_id');
+    const handleChange=(event)=>{
+        setStudentData({
+            ...studentData,
+            [event.target.name]:event.target.value
+        });
+    };
+    const submitForm=()=>{
+        const teacherFormData =new FormData();
+        teacherFormData.append("password", studentData.password);
+        
+
+        try{
+            axios.post(baseUrl+'/student/change_password/'+student_id +'/',teacherFormData).then((response)=>{
+                if(response.status===200){
+                    Swal.fire({
+                        title: 'password hsa been changed',
+                        icon: 'success',
+                        toast:true,
+                        timer:3000,
+                        position:"center",
+                        timerProgressBar:true,
+                        showConfirmButton:false
+                      }
+                    );
+                    window.location.href='/student_logout'
+                }
+            });
+        }catch(error){
+            setStudentData({'status':'error'});
+        }
+        
+    }; 
+    const teacherLoginStatus= localStorage.getItem('teacherLoginStatus');//if the user is logied in redirect them to teacher dashboard.
+    if(teacherLoginStatus!=='true'){
+        window.location.href='/teacher_login'
+    }
+
+
   return (
+
+
     <div className="container mt-4">
             <div className="row">
                 <aside className='col-md-3'>
@@ -11,27 +71,27 @@ const ChangePassword = () => {
                 <div className="card"> 
                     <h4 className="card-header">Change Password</h4>   
                     <div className="card-body">
-                    <div class="mb-2 row">
+                    {/* <div class="mb-2 row">
                         <label for="staticEmail" class="col-sm-2 col-form-label">Old Password</label>
                         <div class="col-sm-10">
                         <input type="text" readonly class="form-control" id="staticEmail" />
                         </div>
-                    </div>
+                    </div> */}
                     <div class="mb-3 row">
                         <label for="inputPassword" class="col-sm-2 col-form-label">New Password</label>
                         <div class="col-sm-10">
-                        <input type="password" class="form-control" id="inputPassword"/>
+                        <input onChange={handleChange}  name ="password" type="text" class="form-control" id="inputPassword"/>
                         </div>
                     </div>
-                    <div class="mb-3 row">
+                    {/* <div class="mb-3 row">
                         <label for="inputPassword" class="col-sm-2 col-form-label">Confirme Password</label>
                         <div class="col-sm-10">
                         <input type="password" class="form-control" id="inputPassword"/>
                         </div>
-                    </div>
+                    </div> */}
                     <hr />
                     <div >
-                    <button type="button" class="btn btn-primary">Update</button>                    
+                    <button type="button" class="btn btn-primary" onClick={submitForm}>Change</button>                    
                     </div>
                     </div>    
                 </div>
--- e-learning-front-end/src/components/User_Dashboard/dashboard.jsx
+++ e-learning-front-end/src/components/User_Dashboard/dashboard.jsx
@@ -1,6 +1,39 @@
 import React from "react";
+import {Link} from 'react-router-dom';
+import axios from 'axios';
+import {useState,useEffect} from "react";
 import Sidebar from "./Sidebar";
+
+
+const baseUrl='http://127.0.0.1:8000/api';
+
+
+
+
+
 const Dashboard = () => {
+    const[dashboar,setDashboar]=useState([]);
+    const student_id =localStorage.getItem('student_id');
+
+    useEffect(()=>{
+      //fetch courses
+      try{
+          axios.get(baseUrl+'/student/dashboard/'+student_id).then((response)=>{//geting teacher by id
+           console.log(response.data);
+           setDashboar(response.data);
+          });
+
+      }catch(error){
+          console.log(error);
+      }
+  },[student_id]);
+
+  const studentLoginStatus = localStorage.getItem('studentLoginStatus')
+  if(studentLoginStatus!== 'true'){
+    window.location.href='/login'
+  }
+
+
   return (
     <div className="container mt-4">
         <div className="row">
@@ -8,9 +41,33 @@ const Dashboard = () => {
                 <Sidebar/>
             </aside>
             <section className="col-md-9">
-              <div className="card">
-                <h5 className="card-header ">Dashboard</h5>
-              </div>  
+               <div className="row">
+                  <div className="col-md-4">
+                    <div className="card border-primary">
+                      <h4 className="card-header bg-primary text-white">Total Enrolled Courses</h4>
+                      <div className="card-body">
+                        <h3><Link to={"/my-courses"}>{dashboar.total_enrolled_courses}</Link></h3>
+                      </div>
+                    </div>   
+                  </div>
+                  <div className="col-md-4">
+                    <div className="card border-primary">
+                      <h4 className="card-header bg-success text-white">Total Favorite Courses </h4>
+                      <div className="card-body">
+                        <h3><Link to={"/favorite-courses"}>{dashboar.total_favorite_courses}</Link></h3>
+                      </div>
+                    </div>   
+                  </div>
+                  <div className="col-md-4">
+                    <div className="card border-primary">
+                      <h4 className="card-header bg-warning text-white">Assignments</h4>
+                      <div className="card-body">
+                        <h5>Complted: <Link to={"/my_assignment"}>{dashboar.total_completed_assignments}</Link></h5>
+                        <h5>Pading: <Link to={"/my_assignment"}>{dashboar.total_pending_assignment}</Link></h5>
+                      </div>
+                    </div>   
+                  </div>
+               </div>
             </section>
         </div>
     </div>
