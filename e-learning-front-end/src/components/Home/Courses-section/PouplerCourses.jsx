4366f69189f51ef2c1a3e4907b543a04527ffc9f
RachidAifar <90982692+RachidAifar@users.noreply.github.com>
2023-02-17 19:36:48 +0100

    feat<component>:poupler teach and courses

--- e-learning-front-end/src/components/Home/Courses-section/PouplerCourses.jsx
+++ e-learning-front-end/src/components/Home/Courses-section/PouplerCourses.jsx
@@ -1,70 +1,90 @@
 import React from "react";
-import {  Container,Row, Col } from "reactstrap";
-import {  ourCoursesData} from "../../Data/Data";
-import CourseCard from "./CourseCard";
+import { useEffect,useState } from "react";
+import {Link} from 'react-router-dom';
 import "./courses.css";
+import axios from 'axios';
 
 
-
+const baseUrl='http://127.0.0.1:8000/api';
 
 
 
 const PouplerCourses = () => {
+    const [courseData,setCourseData] =useState(null);
+   
+    useEffect(()=>{
+    try{
+        axios.get(baseUrl+'/popular_courses/?all=1').then((response)=>{
+            setCourseData(response.data);
+        });
+    }catch(error){
+        console.log(error);
+    }
+      
+    },[]);
+
+
+
+
+
     return (
-        <section>
-        <Container className="container">
-            <Row className="pt-5">
-                <Col lg="12" className="mb-5">
-                <div className="course__top d-flex justify-content-between align-items-center">
-                    <div className="course__top__left w-50">
-                    <h4>Poupler Courses</h4>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
-                        consequatur libero quod voluptatibus ullam quia quas, vitae
-                        voluptatem recusandae reprehenderit!
+        <>
+        <h3 className="pb-1 mt-4 ms-5">Popular Courses</h3>
+        <div className="container  mt-4 me-1">
+            <div className="row mb-5 mt-4 me-1">
+            {courseData && courseData.map((row,index)=>
+                <div className="col-sm-3 mb-4 me-4" >
+                <div className="course_card">
+                <Link to={`/CourseDetail/${row.course.course_id}`}><img className="card-img-top" src={row.course.feature_img} alt={row.course.course_title}/></Link>
+                <div className="card-body">
+                    <h5 className="card-title"><Link to={`/CourseDetail/${row.course.course_id}`}>{row.course.course_title}</Link></h5>
+                    <div className=" d-flex justify-content-between align-items-center">
+                    <p className="lesson d-flex align-items-center gap-1">
+                        <i className="ri-book-open-line"></i>  Lessons
+                    </p>
+
+                    <p className="students d-flex align-items-center gap-1">
+                        <i className="ri-user-line"></i> {row.course.course_views}
+                    </p>
+                    </div>
+
+                    <div className=" d-flex justify-content-between align-items-center">
+                    <p className="rating d-flex align-items-center gap-1">
+                        <i className="ri-star-fill"></i> <Link className="btn btn-info btn-sm">Rating:{row.rating}</Link> 
+                    </p>
+
+                    <p className="enroll d-flex align-items-center gap-1">
+                        <Link to={`/CourseDetail/${row.course.course_id}`}>Enroll Now</Link> 
                     </p>
                     </div>
                 </div>
-                </Col>
-                {ourCoursesData.map((item) => (
-                <Col lg="4" md="6" sm="6" mb="4" onClick={() => this.goToDetails(item)}>
-                    <CourseCard key={item.id} item={item} />
-                </Col>
-                ))}
-                {ourCoursesData.map((item) => (
-                <Col lg="4" md="6" sm="6" onClick={() => this.goToDetails(item)}>
-                <CourseCard key={item.id} item={item} />
-                </Col>
-                ))}
-                {ourCoursesData.map((item) => (
-                <Col lg="4" md="6" sm="6" onClick={() => this.goToDetails(item)}>
-                <CourseCard key={item.id} item={item} />
-                </Col>
-                ))}
-            </Row>
-        </Container>
-        {/* pagination */}
-        <nav aria-label="Page navigation example mt-5">
-            <ul className="pagination justify-content-center">
-                <li className="page-item">
-                <a className="page-link" href="/" aria-label="Previous">
-                    <span aria-hidden="true">&laquo;</span>
-                </a>
-                </li>
-                <li className="page-item"><a class="page-link" href="/">1</a></li>
-                <li className="page-item"><a class="page-link" href="/">2</a></li>
-                <li className="page-item"><a class="page-link" href="/">3</a></li>
-                <li className="page-item">
-                <a className="page-link" href="/" aria-label="Next">
-                    <span aria-hidden="true">&raquo;</span>
-                </a>
-                </li>
-            </ul>
-        </nav>
+                </div>
 
+                </div>
+            )}
+            </div>
+            <nav aria-label="Page navigation example mt-5">
+                <ul className="pagination justify-content-center">
+                    <li className="page-item">
+                        <a className="page-link" href="/" aria-label="Previous">
+                        <span aria-hidden="true">&laquo;</span>
+                        </a>
+                    </li>
+                    <li className="page-item"><a class="page-link" href="/">1</a></li>
+                    <li className="page-item"><a class="page-link" href="/">2</a></li>
+                    <li className="page-item"><a class="page-link" href="/">3</a></li>
+                    <li className="page-item">
+                        <a className="page-link" href="/" aria-label="Next">
+                        <span aria-hidden="true">&raquo;</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+           
+         
 
-
-      </section>
+        </div>
+        </>
     );
 };
 
--- e-learning-front-end/src/components/Home/Courses-section/PouplerTeacher.jsx
+++ e-learning-front-end/src/components/Home/Courses-section/PouplerTeacher.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import { useEffect,useState } from "react";
-import axios from 'axios'
+import axios from 'axios';
+import { Link } from "react-router-dom";
+import {  Col } from "reactstrap";
 
 
-import {  Container,Row, Col } from "reactstrap";
-import {  PouplerCoursesData} from "../../Data/Data";
-import TeacherCards from "./TeacherCards";
 import "./courses.css";
 
 
@@ -14,49 +13,49 @@ import "./courses.css";
 
 const baseUrl='http://127.0.0.1:8000/api'; 
 const PouplerTeacher = () => {
-    const [teacher,setTeacher] =useState(null);
+    const [teacherData,setTeacher] =useState(null);
     useEffect(()=>{
-       axios.get(baseUrl+'/teacher/').then((response)=>{
+       axios.get(baseUrl+'/popular_teachers/?all=1/').then((response)=>{
             setTeacher(response.data)
        });
     },[]);
-    console.log(teacher);
     return (
-        <section>
-        <Container className="container">
-            <Row className="pt-5">
-                <Col lg="12" className="mb-5">
-                <div className="course__top d-flex justify-content-between align-items-center">
-                    <div className="course__top__left w-50">
-                    <h4>Poupler Teachers </h4>
-                    <p>
-                        Lorem ipsum Rachid dolor sit amet consectetur adipisicing elit. Quae
-                        consequatur libero quod voluptatibus ullam quia quas, vitae
-                        voluptatem recusandae reprehenderit!
-                    </p>
-                    </div>
-                </div>
-                </Col>
-                {PouplerCoursesData.map((item) => (
-                <Col lg="4" md="6" sm="6" mb="4" onClick={() => this.goToDetails(item)}>
-                    <TeacherCards key={item.id} item={item} />
-                </Col>
-                ))}
-                {PouplerCoursesData.map((item) => (
-                <Col lg="4" md="6" sm="6" onClick={() => this.goToDetails(item)}>
-                <TeacherCards key={item.id} item={item} />
-                </Col>
-                ))}
-                {PouplerCoursesData.map((item) => (
-                <Col lg="4" md="6" sm="6" onClick={() => this.goToDetails(item)}>
-                <TeacherCards key={item.id} item={item} />
-                </Col>
-                ))}
-            </Row>
-        </Container>
+       <>
+        <h3 className="pb-1 mt-4 ms-4">Teachers</h3>
+            <p className="mt-2 ms-5">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
+                  consequatur libero quod voluptatibus ullam quia quas, vitae
+                  voluptatem recusandae reprehenderit!
+                </p>
+            <div className="container  mt-4 me-1">
+            <div className="row mb-4 ms-4">
+              {teacherData && teacherData.map((raw,index)=>
+              <div className="col-sm-3 mb-4 me-4" >
+                  <div className="course_card mb-5">
+                    <Link to={`/CourseDetail/${raw.teacher_id}`}><img className="card-img-top " src={raw.teacher_profile} alt={raw.teacher_fullname}/></Link>
+                      <div className="card-body mb-5">
+                        <h5 className="card-title mr-0 mt-2"><Link to={`/CourseDetail/${raw.teacher_id}`}>{raw.teacher_fullname}</Link></h5>
+                        <div className="d-flex justify-content-between align-items-center mt-3">
+                          <p className="lesson d-flex align-items-center gap-1">
+                              <i className="ri-book-open-line"></i> Lessons
+                          </p>
+                          {/* <p className="enroll d-flex align-items-center gap-1">
+                              <Link to={"/CourseDetail/1"}>Enroll Now</Link> 
+                          </p> */}
+
+                          <p className="students d-flex align-items-center gap-1">
+                          <i className="ri-user-line"></i><Link to={`/teacher_detail/${raw.teacher_id}`}>Deatil</Link> 
+                          </p>
+                        </div>
+                      </div>
+                  </div>
+                  </div>
+              )}
+            </div>
+            
         {/* pagination */}
         <nav aria-label="Page navigation example mt-5">
-            <ul className="pagination justify-content-center">
+            <ul className="pagination justify-content-center ml-5">
                 <li className="page-item">
                 <a className="page-link" href="/" aria-label="Previous">
                     <span aria-hidden="true">&laquo;</span>
@@ -73,9 +72,9 @@ const PouplerTeacher = () => {
             </ul>
         </nav>
 
+    </div>
 
-
-      </section>
+    </>
     );
 };
 
