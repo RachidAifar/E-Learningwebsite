7b779eb567f1cef25441078e001b5151aab6db92
RachidAifar <90982692+RachidAifar@users.noreply.github.com>
2023-03-28 16:15:19 +0200

    feat<component>:data valdition has been implemented

--- e-learning-front-end/src/components/Authontication_Forms/Login_form.jsx
+++ e-learning-front-end/src/components/Authontication_Forms/Login_form.jsx
@@ -80,18 +80,8 @@ const Login = () => {
                                     <label className="form-label" htmlFor="form3Example4">Password</label>
                                 </div>
 
-                                {/* <div className="d-flex justify-content-between align-items-center">
-                                    <div className="form-check mb-0">
-                                        <input className="form-check-input me-2" type="checkbox" value="" id="form2Example3" />
-                                        <label className="form-check-label" htmlFor="form2Example3">
-                                            Remember me
-                                        </label>
-                                    </div>
-                                    <Link to={"#!"} className="text-body">Forgot password?</Link>
-                                </div> */}
-
-                                <div className="text-center text-md-start mt-4 pt-2">
-                                    <button type="button" onClick={submitForm} className="btn btn-primary btn-sm">Login</button>
+                                <div className="text-center text-md-start mt-2 pt-2">
+                                    <button type="button" onClick={submitForm} className="btn btn-primary btn-sm float-end">Login</button>
                                     <p className="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <Link to={"/register"}
                                         className="link-danger">Register</Link></p>
                                 </div>
--- e-learning-front-end/src/components/Authontication_Forms/Register_form.jsx
+++ e-learning-front-end/src/components/Authontication_Forms/Register_form.jsx
@@ -15,22 +15,59 @@ const Register = () => {
         'address':'',
         'status':''
     });
+    const [errors, setErrors] = useState({});
     //change element value
     const handleChange=(event)=>{
         setStudentData({
             ...studentData,
             [event.target.name]:event.target.value
         });
+        setErrors({
+            ...errors,
+            [event.target.name]: ''
+          });
     };
+    const validateData = () => {
+        let errors = {};
+    
+        if (!studentData.student_fullname) {
+          errors.student_fullname = 'Name is required';
+        }
+    
+        if (!studentData.email) {
+          errors.email = 'Email is required';
+        } else if (!/\S+@\S+\.\S+/.test(studentData.email)) {
+          errors.email = 'Email is invalid';
+        }
+    
+        if (!studentData.password) {
+          errors.password = 'Password is required';
+        } else if (studentData.password.length < 8) {
+          errors.password = 'Password must be at least 8 characters';
+        }
+        if (!studentData.mobile_phone) {
+            errors.mobile_phone = "Mobile Phone is required";
+          } else if (!/^[0-9]+$/.test(studentData.mobile_phone)) {
+            errors.mobile_phone = "Mobile Phone is invalid";
+          }
+          if (!studentData.address) {
+            errors.address = "Address is required";
+          }
+    
+          return errors;
+      };
     //end
     const submitForm=()=>{
         const studentFormData =new FormData();
+        const errors = validateData();
+        setErrors(errors);
         studentFormData.append("student_fullname", studentData.student_fullname)
         studentFormData.append("password", studentData.password)
         studentFormData.append("email", studentData.email)
         studentFormData.append("mobile_phone", studentData.mobile_phone)
         studentFormData.append("address", studentData.address)
         try{
+            if (Object.keys(errors).length === 0){
             axios.post(baseUrl,studentFormData,).then((response)=>{
                 setStudentData({
                     'student_fullname':'',
@@ -40,7 +77,7 @@ const Register = () => {
                     'address':'',
                     'status':'success'
                 });
-            });
+            })};
         }catch(error){
             setStudentData({'status':'error'});
         }
@@ -63,36 +100,77 @@ const Register = () => {
                                 <div className="card-body">
                                     <h2 className="text-uppercase text-center mb-4">Create an account</h2>
                                     
-                                        <div className="form-outline mb-2">
-                                            <label className="form-label" htmlFor="form3Example1cg">Name</label>
-                                            <input type="text"  name="student_fullname" onChange={handleChange} value={studentData.student_fullname} id="form3Example1cg" className="form-control form-control-sm" />
-                                        </div>
+                                    <div className="form-outline mb-2">
+                                        <label className="form-label" htmlFor="form3Example1cg">
+                                            Name
+                                        </label>
+                                        <input
+                                            type="text"
+                                            name="student_fullname"
+                                            onChange={handleChange}
+                                            value={studentData.student_fullname}
+                                            id="form3Example1cg"
+                                            className={`form-control form-control-sm ${
+                                            errors.student_fullname ? "is-invalid" : ""
+                                            }`}
+                                        />
+                                        {errors.student_fullname && (
+                                            <div className="invalid-feedback">{errors.student_fullname}</div>
+                                        )}
+                                    </div>
+                                   <div className="form-outline mb-2">
+                                        <label className="form-label" htmlFor="form3Example3cg">
+                                            Email
+                                        </label>
+                                        <input
+                                            type="email"
+                                            name="email"
+                                            onChange={handleChange}
+                                            value={studentData.email}
+                                            id="form3Example3cg"
+                                            className={`form-control form-control-sm ${
+                                            errors.email ? "is-invalid" : ""
+                                            }`}
+                                        />
+                                        {errors.email && <div className="invalid-feedback">{errors.email}</div>}
+                                    </div>
 
-                                        <div className="form-outline mb-2">
-                                            <label className="form-label" htmlFor="form3Example3cg">Email</label>
-                                            <input type="email" name="email" onChange={handleChange} value={studentData.email} id="form3Example3cg" className="form-control form-control-sm" />
-                                        </div>
+                                    <div className="form-outline mb-2">
+                                        <label className="form-label" htmlFor="form3Example4cg">
+                                            Password
+                                        </label>
+                                        <input
+                                            type="password"
+                                            name="password"
+                                            onChange={handleChange}
+                                            value={studentData.password}
+                                            id="form3Example4cg"
+                                            className={`form-control form-control-sm ${
+                                            errors.password ? "is-invalid" : ""
+                                            }`}
+                                        />
+                                        {errors.password && (
+                                            <div className="invalid-feedback">{errors.password}</div>
+                                        )}
+                                    </div>
+                                    <div className="form-outline mb-2">
+                                        <label className="form-label" htmlFor="form3Example1cg">Mobile Phone</label>
+                                        <input type="text" name="mobile_phone" onChange={handleChange} value={studentData.mobile_phone} id="form3Example1cg" className={`form-control form-control-sm ${errors.mobile_phone && 'is-invalid'}`} />
+                                        {errors.mobile_phone && <div className="invalid-feedback">{errors.mobile_phone}</div>}      
+                                    </div>
 
-                                        <div className="form-outline mb-2">
-                                            <label className="form-label" htmlFor="form3Example4cg">Password</label>
-                                            <input type="password" name="password" onChange={handleChange} value={studentData.password} id="form3Example4cg" className="form-control form-control-sm" />
-                                        </div>
-                                        <div className="form-outline mb-2">
-                                            <label className="form-label" htmlFor="form3Example1cg">Mobile Phone</label>
-                                            <input type="text" name="mobile_phone" onChange={handleChange} value={studentData.mobile_phone}  id="form3Example1cg" className="form-control form-control-sm" />
-                                        </div>
-                                        <div className="form-outline mb-2">
-                                            <label className="form-label" htmlFor="form3Example3cg">Address</label>
-                                            <textarea   name="address" onChange={handleChange} value={studentData.address} type="text" id="form3Example3cg" className="form-control" />
-                                        </div>
+                                    <div className="form-outline mb-2">
+                                        <label className="form-label" htmlFor="form3Example3cg">Address</label>
+                                        <textarea   name="address" onChange={handleChange} value={studentData.address} type="text" id="form3Example3cg" className={`form-control form-control-sm ${errors.address && 'is-invalid'}`} />
+                                    </div>
                                         
-                                        <div className="d-flex justify-content-center">
-                                            <button type="button" onClick={submitForm}className="btn btn-success btn-block btn-lg gradient-custom-4 text-body">Register</button>
-                                        </div>
+                                    <div className="d-flex justify-content-center">
+                                        <button type="button" onClick={submitForm} className="btn btn-success btn-block btn-lg gradient-custom-4 text-body">Register</button>
+                                    </div>
 
-                                        <p className="text-center text-muted mt-2 mb-0">Have already an account? <Link to={"/login"}
-                                            className="fw-bold text-body"><u>Login here</u></Link>
-                                        </p>  
+                                    <p className="text-center text-muted mt-2 mb-0">Have already an account? <Link to={"/login"}
+                                        className="fw-bold text-body"><u>Login here</u></Link>
+                                    </p>  
                                 </div>
                             </div>
                         </div>
